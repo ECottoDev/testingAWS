7ce7473cc6934f13dc4797ec6b43b203903d0373
Luxian1 <edtowers1037@gmail.com>
2024-06-01 21:31:38 -0400

    Revert "login2"
    This reverts commit 8551d1d8d660f669eaef2aee1f8549b872806df9.

--- frontend/views/loginView/LoginView.js
+++ frontend/views/loginView/LoginView.js
@@ -30,6 +30,6 @@ export class LoginView {
 
     async submitLogin() {
         //await systemLogin(this.username.value, this.password.value);
-        console.log(await getUsers());
+        await getUsers();
     }
 }
